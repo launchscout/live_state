c93077d7eef69d84944c2e899ab8c333feb73032
Chris Nelson <cnelson@blvd.co>
2022-07-04 14:40:33 -0400

    better documentation

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule LiveState.MixProject do
   use Mix.Project
 
+  @description "A package to manage web application state"
+
   def project do
     [
       app: :live_state,
@@ -8,7 +10,12 @@ defmodule LiveState.MixProject do
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      package: [
+        licenses: ["MIT"],
+        links: %{"Github" => "https://github.com/gaslight/live_state"}
+      ]
     ]
   end
 
@@ -26,6 +33,7 @@ defmodule LiveState.MixProject do
   defp deps do
     [
       {:phoenix, ">= 1.5.7"},
+      {:ex_doc, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
