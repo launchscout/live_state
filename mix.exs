2a027a25dec765fa13d6433b5f349a0c795a43bf
Chris Nelson <superchrisnelson@gmail.com>
2022-08-02 14:05:06 -0400

    messed up dep

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LiveState.MixProject do
   def project do
     [
       app: :live_state,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -33,7 +33,8 @@ defmodule LiveState.MixProject do
   defp deps do
     [
       {:phoenix, ">= 1.5.7"},
-      {:ex_doc, ">= 0.0.0"}
+      {:ex_doc, ">= 0.0.0"},
+      {:json_diff, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
