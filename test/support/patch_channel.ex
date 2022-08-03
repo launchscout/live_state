6f2692c93561ca7e9784955a4a6de8a48c13b469
Chris Nelson <chris@gaslight.co>
2022-08-03 17:53:27 -0400

    adding json_patch option

---
+++ test/support/patch_channel.ex
@@ -0,0 +1,11 @@
+defmodule LiveState.Test.PatchChannel do
+  use LiveState.Channel, web_module: LiveState.Test.Web, json_patch: true
+
+  def init(_channel, _params, _socket) do
+    {:ok, %{foo: "bar"}}
+  end
+
+  def handle_event("change_foo", %{"foo" => new_foo}, _state) do
+    {:noreply, %{foo: new_foo}}
+  end
+end
