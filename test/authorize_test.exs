85e62e04136e276c7dc1f78730b3b1158c1ebe87
Chris Nelson <superchrisnelson@gmail.com>
2022-09-05 16:08:32 -0400

    Added authorize and bumped version

---
+++ test/authorize_test.exs
@@ -0,0 +1,26 @@
+defmodule LiveState.AuthorizeTest do
+  use ExUnit.Case
+
+  import Phoenix.ChannelTest
+  alias LiveState.Test.AuthorizedChannel
+  alias LiveState.Test.UserSocket
+
+  @endpoint LiveState.Test.Endpoint
+
+  setup do
+    start_supervised(@endpoint)
+    start_supervised(Phoenix.PubSub.child_spec(name: LiveState.Test.PubSub))
+
+    {:ok, %{socket: socket(UserSocket, "wut", %{})}}
+  end
+
+  test "successful join", %{socket: socket} do
+    assert {:ok, _result, _socket} = socket |> subscribe_and_join("authorized:all", %{"password" => "secret"})
+    assert_push("state:change", %{state: %{authorized: true}, version: 0})
+  end
+
+  test "unsuccessful join", %{socket: socket} do
+    assert {:error, reason} = socket |> subscribe_and_join("authorized:all", %{"password" => "garbage"})
+  end
+
+end
