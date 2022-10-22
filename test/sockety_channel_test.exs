8b636ef05fc0a692807ca78cb59eb0029a332af1
Chris Nelson <superchrisnelson@gmail.com>
2022-10-22 19:05:45 -0400

    added auth

---
+++ test/sockety_channel_test.exs
@@ -0,0 +1,38 @@
+defmodule LiveState.SocketyChannelTest do
+  use ExUnit.Case
+
+  import Phoenix.ChannelTest
+  alias LiveState.Test.SocketyChannel
+  alias LiveState.Test.UserSocket
+
+  @endpoint LiveState.Test.Endpoint
+
+  setup do
+    start_supervised(@endpoint)
+    start_supervised(Phoenix.PubSub.child_spec(name: LiveState.Test.PubSub))
+
+    {:ok, _, socket} =
+      socket(UserSocket, "wut", %{})
+      |> subscribe_and_join(SocketyChannel, "sockety:sock", %{})
+
+    {:ok, %{socket: socket}}
+  end
+
+  test "init", %{socket: %{assigns: %{baz: baz}}} do
+    assert baz == "bing"
+    assert_push(
+      "state:change",
+      %{state: %{foo: "bar"}, version: 0}
+    )
+  end
+
+  test "handle_event", %{socket: socket} do
+    push(socket, "lvs_evt:something_sockety", %{"baz" => "wuzzle"})
+
+    assert_push("state:change", %{
+      state: %{foo: "altered bar"},
+      version: 1
+    })
+  end
+
+end
