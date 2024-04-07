57dd44b1307e1cb12aae43055a829cbe4db3846a
Chris Nelson <superchrisnelson@gmail.com>
2024-04-07 17:42:20 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LiveState.MixProject do
   def project do
     [
       app: :live_state,
-      version: "0.7.2",
+      version: "0.8.0",
       elixir: ">= 1.12.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
